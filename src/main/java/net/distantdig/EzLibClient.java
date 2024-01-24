c423ed187cda9609190d72c3a3e954d53db6f421
DistantDig <jameswaller711@gmail.com>
2024-01-24 14:51:28 -0800

    Initial commit

---
+++ src/main/java/net/distantdig/EzLibClient.java
@@ -0,0 +1,10 @@
+package net.distantdig;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EzLibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/distantdig/EzLibDataGenerator.java
@@ -0,0 +1,11 @@
+package net.distantdig;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EzLibDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
