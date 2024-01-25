c5de1ec7a0b83507751e4eb002638c69a897d1da
DistantDig <jameswaller711@gmail.com>
2024-01-24 17:00:45 -0800

    changed mappings to mojang mappings

--- src/main/java/net/distantdig/item/EzItems.java
+++ src/main/java/net/distantdig/item/EzItems.java
@@ -4,27 +4,27 @@ import net.distantdig.EzLib;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 
 public class EzItems {
 
     public static final Item EZ_EXAMPLE_ITEM = registerItem("ez_example_item", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
-        entries.add(EZ_EXAMPLE_ITEM);
+        entries.accept(EZ_EXAMPLE_ITEM);
     }
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(EzLib.MOD_ID, name), item);
+        return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(EzLib.MOD_ID, name), item);
     }
 
     public static void regigisterEzItems() {
         EzLib.LOGGER.info("Registering Ez Items");
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(EzItems::addItemsToIngredientTabItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS).register(EzItems::addItemsToIngredientTabItemGroup);
     }
 }
---
+++ src/main/java/net/distantdig/item/ItemRegister.java
@@ -0,0 +1,14 @@
+package net.distantdig.item;
+
+import net.minecraft.world.item.Item;
+
+import java.util.HashMap;
+
+public class ItemRegister {
+
+    public final static HashMap<String, Item> itemMap = new HashMap<>();
+
+//    public static <T extends Item> void registerItem(String key, Function<BlockBehavior.Properties, T> ctor, BlockBehavior.Properties props) {
+//
+//    }
+}
