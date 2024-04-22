96285f743b2121289d3ef53c1259133671676f7e
DistantDig <jameswaller711@gmail.com>
2024-04-22 11:21:51 -0700

    testing stair and slab model generation

--- src/main/java/net/distantdig/ezLib/datagen/EzModelProvider.java
+++ src/main/java/net/distantdig/ezLib/datagen/EzModelProvider.java
@@ -8,7 +8,10 @@ import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
 import net.minecraft.data.models.model.ModelTemplates;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.ArrayList;
 
@@ -25,8 +28,16 @@ public class EzModelProvider extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
         EzBlocksBuilder.blockMap.forEach((strings, block) -> {blockStateModelGenerator.createTrivialCube(block);});
-        EzBlocksBuilder.stairMap.forEach((strings, block) -> {JsonGenerators.createEzStair(strings);});
-        EzBlocksBuilder.slabMap.forEach((strings, block) -> {JsonGenerators.createEzSlab(strings);});
+        EzBlocksBuilder.stairMap.forEach((strings, block) -> {
+            BlockModelGenerators.BlockFamilyProvider blockPool = blockStateModelGenerator.family(EzBlocksBuilder.blockMap.get(strings.fullblockname));
+
+            blockPool.stairs(block);
+        });
+        EzBlocksBuilder.slabMap.forEach((strings, block) -> {
+            BlockModelGenerators.BlockFamilyProvider blockPool = blockStateModelGenerator.family(EzBlocksBuilder.blockMap.get(strings.fullblockname));
+
+            blockPool.slab(block);
+        });
         EzBlocksBuilder.verticalSlabMap.forEach((strings, block) -> {JsonGenerators.createEzVerticalSlab(strings);});
         EzBlocksBuilder.leavesMap.forEach((strings, block) -> {JsonGenerators.createEzBlock(strings);});
         EzBlocksBuilder.rotatedPillarMap.forEach((strings, block) -> {JsonGenerators.createEzColumn(strings);});
@@ -39,6 +50,8 @@ public class EzModelProvider extends FabricModelProvider {
         EzBlocksBuilder.wallMap.forEach((strings, block) -> {JsonGenerators.createEzWall(strings);});
         EzBlocksBuilder.pressurePlateMap.forEach((strings, block) -> {JsonGenerators.createEzPressurePlate(strings);});
         EzBlocksBuilder.carpetMap.forEach((strings, block) -> {JsonGenerators.createEzCarpet(strings);});
+
+
     }
 
     @Override
@@ -48,5 +61,14 @@ public class EzModelProvider extends FabricModelProvider {
         FlatModelList.forEach((item -> itemModelGenerator.generateFlatItem(item, ModelTemplates.FLAT_ITEM)));
         HandheldModelList.forEach((item -> itemModelGenerator.generateFlatItem(item, ModelTemplates.FLAT_HANDHELD_ITEM)));
         ArmorModelList.forEach((armorData -> itemModelGenerator.generateArmorTrims(armorData.armorItem)));
+    }
+
+    public BlockModelGenerators.BlockFamilyProvider stairs(Block block) {
+        ResourceLocation resourceLocation = getOrCreateModel(ModelTemplates.STAIRS_INNER, block);
+        ResourceLocation resourceLocation2 = this.getOrCreateModel(ModelTemplates.STAIRS_STRAIGHT, block);
+        ResourceLocation resourceLocation3 = this.getOrCreateModel(ModelTemplates.STAIRS_OUTER, block);
+        BlockModelGenerators.this.blockStateOutput.accept(BlockModelGenerators.createStairs(block, resourceLocation, resourceLocation2, resourceLocation3));
+        BlockModelGenerators.this.delegateItemModel(block, resourceLocation2);
+        return this;
     }
 }
